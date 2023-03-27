9e352d046bcbbe278a32b862757e956af0e11d53
SPmfl <matheo.franco.loaiza@gmail.com>
2023-03-27 17:22:20 -0500

    features crud operations working

--- src/controllers/marks.controllers.js
+++ src/controllers/marks.controllers.js
@@ -4,70 +4,84 @@ const { Op } = require('sequelize');
 
 const markController = {};
 
-markController.getAllFeatures = async (req, res)=>{
+markController.getAllFeatures = async (req, res) => {
     try {
         const allFeatures = await Feature.findAll();
         res.status(200).json(allFeatures);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "server error - can't retrieve Features"});
+        res.status(500).json({ msg: "server error - can't retrieve Features" });
     }
 }
 
-markController.getFeature = async (req, res)=>{
+markController.getFeature = async (req, res) => {
     try {
         const id = req.params.id;
-        if( id === 0 || id === undefined ){
-            res.status(400).json({msg: "id not provided or wrong format"});    
+        if (id === 0 || id === undefined) {
+            res.status(400).json({ msg: "id not provided or wrong format" });
         }
-        const allFeatures = await Feature.findAll({
+        const feature = await Feature.findAll({
             where: { id: { [Op.eq]: id } }
         });
-        res.status(200).json(allFeatures);
+        res.status(200).json(feature);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "server error - can't retrieve the Feature"});
+        res.status(500).json({ msg: "server error - can't retrieve the Feature" });
     }
-} 
+}
 
-markController.createFeature = async (req, res)=>{
+markController.createFeature = async (req, res) => {
     try {
-        // console.log(req.body);
-        // res.send("createFeature")
         const { id, type, geometry } = req.body;
-        if( id && type && geometry){
+        if (id && type && geometry) {
             const newFeature = await Feature.create({ id, type, geometry });
             res.status(200).json(newFeature);
             console.log("feature created");
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "server error - can't create Feature"});
+        res.status(500).json({ msg: "server error - can't create Feature" });
     }
-
 }
 
-markController.deleteFeature = async (req, res)=>{
+markController.deleteFeature = async (req, res) => {
     try {
-        const allFeatures = await Feature.findAll();
-        res.status(200).json(allFeatures);
+        const id = req.params.id;
+        if (verifyFeature(id)) {
+            await Feature.destroy({
+                where: { id: { [Op.eq]: id } }
+            });
+            res.status(200).json({ msg: "Feature deleted" });
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "server error - can't retrieve Features"});
+        res.status(500).json({ msg: "server error - can't delete Feature" });
     }
 }
 
-markController.updateFeature = async (req, res)=>{
+markController.updateFeature = async (req, res) => {
     try {
-        // const updatedFeature = await Feature.update
-        // res.status(200).json(updatedFeature);
+        const {id, type, geometry} = req.body;
+        if(!id) res.status(200).json({msg: "Feature id not provided"});
+        if(verifyFeature(id)){
+            const updatedFeature = await Feature.update({type, geometry},{
+                where: {id: { [Op.eq]: id}}
+            });
+            res.status(200).json(updatedFeature);
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ msg: "server error - can't retrieve a Feature"});
+        res.status(500).json({ msg: "server error - can't update a Feature" });
     }
-
 }
 
+const verifyFeature = async (id) => {
+    const ft = await Feature.findAll({
+        where: { id: { [Op.eq]: id } }
+    });
+    if (ft) return true;
+    return false;
+}
 
 module.exports = markController;
 
